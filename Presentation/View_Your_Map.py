0b5d4f9cd58651493ba2fe37b641f1791fb3fc83
Mikael Weiss <campingmikael@icloud.com>
2024-10-19 16:29:57 -0600

    done

--- Presentation/View_Your_Map.py
+++ Presentation/View_Your_Map.py
@@ -14,57 +14,52 @@ conn = st.connection("supabase", type=SupabaseConnection, ttl=3600)
 # Perform query.
 rows = conn.table("user_financial_data").select("*").execute()
 
-# Print results.
-for row in rows.data:
-    st.write(
-        f"Month: {row['month']}, savings {row['savings']}, income: {row['income']} debt: {row['debt']}")
+# showData = st.toggle("Show data", value=True)
 
-showData = st.toggle("Show data", value=True)
+# if not showData:
+#     st.warning(
+#         """
+#     Oh no! Looks like you haven't added any data yet.
+#     Head over to the "Input Data" page to get started!
 
-if not showData:
-    st.warning(
-        """
-    Oh no! Looks like you haven't added any data yet.
-    Head over to the "Input Data" page to get started!
+#     (Trust me, it's worth it)
+#     """
+#     )
 
-    (Trust me, it's worth it)
+#     st.write(
+#         """
+#     Don't believe me? Here's a sneak peak into what you're missing out on:
+#     """
+#     )
+
+#     st.caption("Soon to be added images of what it will look like")
+# else:
+st.write(
     """
-    )
-
-    st.write(
-        """
-    Don't believe me? Here's a sneak peak into what you're missing out on:
-    """
-    )
-
-    st.caption("Soon to be added images of what it will look like")
-else:
-    st.write(
-        """
 Each of our paths are are probably uphill, so lets keep our eyes on the goal: *Financial freedom*
 """
-    )
-    chart_data = pd.DataFrame(np.array([[0, 2000, 100], [1, 2000, 200], [2, 2000, 300], [3, 2000, 400], [4, 2000, 500]]),
-                              columns=['Months', 'Expenses', 'Current Savings'])
+)
 
-    st.line_chart(chart_data, x='Months')
+chart_data = pd.DataFrame(rows.data,
+                          columns=['month', 'savings', 'income', 'debt'])
+st.line_chart(chart_data, x='month')
 
-    st.header("Years to Financial Freedom: 150 years")
-    st.caption("""
-    There are a lot of ways to define financial freedom, but here, we'll define it as having enough savings to cover your expenses, plus extra for emergencies, and of course, fun! Enough savings to last you the rest of your life.
-             """)
+st.header("Years to Financial Freedom: 150 years")
+st.caption("""
+There are a lot of ways to define financial freedom, but here, we'll define it as having enough savings to cover your expenses, plus extra for emergencies, and of course, fun! Enough savings to last you the rest of your life.
+            """)
 
-    tab1, tab2, tab3, tab4 = st.tabs(["Savings", "Income", "Expenses", "Debt"])
+tab1, tab2, tab3, tab4 = st.tabs(["Savings", "Income", "Expenses", "Debt"])
 
-    with tab1:
-        st.write("Savings")
-        st.write("This is where you can see your savings grow over time")
-    with tab2:
-        st.write("Income")
-        st.write("This is where you can see your income grow over time")
-    with tab3:
-        st.write("Expenses")
-        st.write("This is where you can see your expenses grow over time")
-    with tab4:
-        st.write("Debt")
-        st.write("This is where you can see your debt shrink over time")
+with tab1:
+    st.write("Savings")
+    st.write("This is where you can see your savings grow over time")
+with tab2:
+    st.write("Income")
+    st.write("This is where you can see your income grow over time")
+with tab3:
+    st.write("Expenses")
+    st.write("This is where you can see your expenses grow over time")
+with tab4:
+    st.write("Debt")
+    st.write("This is where you can see your debt shrink over time")
--- Presentation/update_data.py
+++ Presentation/update_data.py
@@ -1,17 +1,20 @@
 import streamlit as st
 import pandas as pd
+from supabase import create_client, Client
+
+# Initialize connection to Supabase
+url = st.secrets.connections.supabase["SUPABASE_URL"]
+key = st.secrets.connections.supabase["SUPABASE_KEY"]
+supabase: Client = create_client(url, key)
+
+# Fetch data from Supabase
+response = supabase.table('user_financial_data').select('*').execute()
+data = response.data
+
+# Convert data to DataFrame
+df = pd.DataFrame(data)
+
 st.title('User Data')
 st.caption('- - -')
 
-
-df = pd.DataFrame(columns=['Month', 'user_income', 'User-saving-APY',
-                  'user-savings', 'user-debt', 'user-debt-intrist'])
-data_money = ({
-    'Month': st.column_config.NumberColumn('Age (years)', min_value=0, max_value=122),
-    'user_income': st.column_config.NumberColumn('Age (years)', min_value=0, max_value=122),
-    'User-saving-APY': st.column_config.NumberColumn('Age (years)', min_value=0, max_value=122),
-    'user-savings': st.column_config.NumberColumn('Age (years)', min_value=0, max_value=122),
-    'user-debt': st.column_config.NumberColumn('Age (years)', min_value=0, max_value=122),
-    'user-debt-intrist': st.column_config.NumberColumn('Age (years)', min_value=0, max_value=122),
-})
 data_money = st.data_editor(df, num_rows='dynamic')
