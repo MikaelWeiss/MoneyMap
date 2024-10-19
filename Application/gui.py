19bc36c250a42ae07ea26a554c875a3e148866f7
“Cooper Brady” <cbra207@wgu.edu>
2024-10-18 18:48:05 -0600

    inital Hackathon Money Mapping AI commit.

---
+++ Application/gui.py
@@ -0,0 +1,13 @@
+import customtkinter
+
+def button_callback():
+    print("button clicked Haha")
+
+app = customtkinter.CTk()
+app.geometry("400x150")
+
+button = customtkinter.CTkButton(app, text="my button", command=button_callback)
+button.pack(padx=20, pady=20)
+
+app.mainloop()
+#notes?
