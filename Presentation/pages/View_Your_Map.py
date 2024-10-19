044124f3d43a4ac01ffd4eb0a17d9304ead3dddc
Mikael Weiss <campingmikael@icloud.com>
2024-10-18 21:45:50 -0600

    Added requirements.txt file

--- Presentation/pages/View_Your_Map.py
+++ Presentation/pages/View_Your_Map.py
@@ -4,3 +4,9 @@ import streamlit as st
 
 st.title("Your Map to Success")
 st.caption("Let's be real. Finances are tough. Don't worry, there's a map to financial freedom for all of us!")
+st.write("""
+# Your Map to Success
+         testing
+         """)
+
+st.text("Sup")
