404b287430b3e77742ef2e0c03bdf80f0eca3d16
“Cooper Brady” <cbra207@wgu.edu>
2024-10-18 19:53:00 -0600

    inital Hackathon Money Mapping AI commit.

--- Application/gui.py
+++ Application/gui.py
@@ -8,6 +8,7 @@ app.geometry("1050x500")
 #Buttons
 button = customtkinter.CTkButton(app, text="my button", command=button_callback)
 button.pack(padx=20, pady=20)
+intialTextBox = customtkinter.CTK
 
 app.mainloop()
 #notes?
