46b1775a79461bb454ab7d1c9ede930a09c9729a
“Cooper Brady” <cbra207@wgu.edu>
2024-10-18 19:44:42 -0600

    inital Hackathon Money Mapping AI commit.

--- Application/gui.py
+++ Application/gui.py
@@ -1,11 +1,11 @@
 import customtkinter
-
+#button function
 def button_callback():
     print("button clicked Haha")
-
+#width and hieght of display
 app = customtkinter.CTk()
-app.geometry("400x150")
-
+app.geometry("1050x500")
+#Buttons
 button = customtkinter.CTkButton(app, text="my button", command=button_callback)
 button.pack(padx=20, pady=20)
 
